396e5efe0b608080489864298419c3f91037937e
venkatakrishna18 <venkatakrishnapurushotham@gmail.com>
2026-02-13 19:35:50 +0530

    ITEM AND PAYMENT  UPDATED

--- EatExpress/src/main/java/com/alpha/EatExpress/Entity/Payment.java
+++ EatExpress/src/main/java/com/alpha/EatExpress/Entity/Payment.java
@@ -7,31 +7,57 @@ import jakarta.persistence.*;
 public class Payment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String method; // UPI, CARD, COD
-    private String status; // SUCCESS, FAILED
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
     private double amount;
-
+    private String type;
+    private String status;
     @OneToOne
-    @JoinColumn(name = "order_id")
-    private Order order;
-
-    public Payment() {}
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getMethod() { return method; }
-    public void setMethod(String method) { this.method = method; }
-
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-
-    public double getAmount() { return amount; }
-    public void setAmount(double amount) { this.amount = amount; }
-
-    public Order getOrder() { return order; }
-    public void setOrder(Order order) { this.order = order; }
+    private FoodOrder foodorder;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public FoodOrder getFoodorder() {
+		return foodorder;
+	}
+	public void setFoodorder(FoodOrder foodorder) {
+		this.foodorder = foodorder;
+	}
+	public Payment(int id, double amount, String type, String status, FoodOrder foodorder) {
+		super();
+		this.id = id;
+		this.amount = amount;
+		this.type = type;
+		this.status = status;
+		this.foodorder = foodorder;
+	}
+	public Payment() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Payment [id=" + id + ", amount=" + amount + ", type=" + type + ", status=" + status + "]";
+	}
+    
 }
