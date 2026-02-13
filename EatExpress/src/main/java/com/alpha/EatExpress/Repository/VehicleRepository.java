77e1ce483d36860a53147cd370fdfabdd917be11
Ravindragamidi <ravindragamidi@gmail.com>
2026-02-13 21:44:10 +0530

    Second commit

--- EatExpress/src/main/java/com/alpha/EatExpress/Repository/VehicleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alpha.EatExpress.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.alpha.EatExpress.Entity.Vehicle;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-}
